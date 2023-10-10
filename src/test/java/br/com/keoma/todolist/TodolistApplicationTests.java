043a038b91260d33f0cfd6f004a48df5ed3c4022
Keoma Kevin Baudin <keoma4@gmail.com>
2023-10-09 23:03:11 -0300

    feat: aula 1

---
+++ src/test/java/br/com/keoma/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.keoma.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
