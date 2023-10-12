7d4ac2d0932dfaa251da17a5d298594c86db2993
Keoma Kevin Baudin <keoma4@gmail.com>
2023-10-12 17:54:14 -0300

    feat: aula 4

--- src/main/java/br/com/keoma/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/keoma/todolist/filter/FilterTaskAuth.java
@@ -50,6 +50,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
       return;
     }
 
+    request.setAttribute("userId", user.getId());
     filterChain.doFilter(request, response);
   }
 
--- src/main/java/br/com/keoma/todolist/task/TaskController.java
+++ src/main/java/br/com/keoma/todolist/task/TaskController.java
@@ -1,10 +1,23 @@
 package br.com.keoma.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import br.com.keoma.todolist.utils.Utils;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -13,8 +26,42 @@ public class TaskController {
   private ITaskRepository taskRepository;
 
   @PostMapping("/")
-  public TaskModel create(@RequestBody TaskModel taskModel) {
+  public ResponseEntity<TaskModel> create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    var userId = (UUID) request.getAttribute("userId");
+    taskModel.setUserId(userId);
+
+    var currentDate = LocalDateTime.now();
+    if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+      throw new ResponseStatusException(
+          HttpStatus.BAD_REQUEST, "A data de início / data de término deve ser maior que a data atual");
+    }
+
+    if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+      throw new ResponseStatusException(
+          HttpStatus.BAD_REQUEST, "A data de início deve ser menor do que a data de término");
+    }
+
     var task = this.taskRepository.save(taskModel);
-    return task;
+    return ResponseEntity.status(HttpStatus.CREATED).body(task);
+  }
+
+  @GetMapping("/")
+  public List<TaskModel> list(HttpServletRequest request) {
+    var userId = (UUID) request.getAttribute("userId");
+    var tasks = this.taskRepository.findByUserId(userId);
+    return tasks;
+  }
+
+  @PatchMapping("/{id}")
+  public TaskModel update(@PathVariable UUID id, @RequestBody TaskModel taskModel) {
+    var task = this.taskRepository.findById(id).orElse(null);
+    if (task == null) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Tarefa não encontrada");
+    }
+
+    Utils.copyNonNullProperties(taskModel, task);
+
+    var updatedTask = this.taskRepository.save(task);
+    return updatedTask;
   }
 }
